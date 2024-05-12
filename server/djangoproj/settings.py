86f48facac3e45e6e4d4f0756af264a30a9fc937
kali <kalid.mo@hotmail.com>
2024-05-12 14:57:31 -0400

    update

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,13 +28,13 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost', 'https://kalidmo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = [
-    'https://kalidmo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
-}
+ALLOWED_HOSTS = ['localhost',
+                 'https://kalidmo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
+                 ]
+CSRF_TRUSTED_ORIGINS = \
+    ['https://kalidmo-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+     ]
+REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES': []}
 
 # Application definition
 
@@ -92,24 +92,15 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = \
+    [{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+     },
+     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'
+     },
+     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'
+     },
+     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'
+     }]
 
 
 # Internationalization
